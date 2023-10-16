00701ae5f58ad0eee65f7b317c02bb489b6283c9
balaji bonageri <74399784+balajibonageri@users.noreply.github.com>
2023-10-16 16:53:15 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div>
             <h2>About</h2>
-            <p>Hey there I'm Balaji, a Front End developer, currently I'm learning ReactJs and activily looking for intership regarding the same.</p>
+            <p>Hey there I'm Balaji, A Front End developer, currently I'm learning ReactJs and actively looking for internship and full time role regarding the same.</p>
         </div>
     );
 }
